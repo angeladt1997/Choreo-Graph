2653fec8af90f360408e5c4141ecbe6c595ffdf4
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-10-02 16:18:32 -0500

    Update piece-api-service.js

--- src/Services/piece-api-service.js
+++ src/Services/piece-api-service.js
@@ -62,21 +62,7 @@ const PieceApiService = {
         ? res.json().then(e => Promise.reject(e))
         : res.json()
     })
-  },
-  // deletePiece(shipId){
-  //   return fetch(`${config.API_ENDPOINT}/Stage/${assignedpieces.Id}`, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       'authorization': `bearer ${TokenService.getAuthToken()}`,
-  //     },
-    // })
-    // .then(res => 
-    //   (!res.ok)
-    //   ? res.json().then(e => Promise.reject(e))
-    //   : res.json()
-    // )
-  //}
+  }
 }
 
 export default PieceApiService
