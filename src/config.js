a1a26371f440d9fd28bb8b867de67a3483de4902
Angela Thomas <66443127+angeladt1997@users.noreply.github.com>
2020-10-05 20:03:41 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://choreo-graph-database.herokuapp.com/',
+  API_ENDPOINT: 'https://choreo-graph-server.herokuapp.com/',
   TOKEN_KEY: 'choreograph-client-auth-token',
 }
